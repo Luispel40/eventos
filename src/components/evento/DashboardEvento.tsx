265e4467efb2989b94fc416f350c60c44ee41e94
Luispel40 <luispel2011@gmail.com>
2024-11-24 00:51:26 -0300

    feat: generate qr code and convidado apnnel

--- src/components/evento/DashboardEvento.tsx
+++ src/components/evento/DashboardEvento.tsx
@@ -1,5 +1,6 @@
 import { Evento } from "@/core";
 import InformacoesEvento from "./InformacoesEvento";
+import AcessarViaQrCode from "./AcessarViaQeCode";
 
 export interface DashboardEventoProps {
   evento: Evento;
@@ -7,9 +8,10 @@ export interface DashboardEventoProps {
 
 export default function DashboardEvento(props: DashboardEventoProps) {
   return (
-    <div>
-      <div>
-        <InformacoesEvento evento={props.evento} />
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2 self-stretch">
+        <InformacoesEvento evento={props.evento} className="flex-1" />
+        <AcessarViaQrCode evento={props.evento} />
       </div>
     </div>
   );
